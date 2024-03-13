bb23cd0e506fac73a8e71a080cd01474a51a8266
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-03-13 16:43:42 -0700

    start mentat interop in javascript

--- src/app/mentat.ts
+++ src/app/mentat.ts
@@ -1,1 +1,17 @@
+async function loadMentatInterface(): Promise<WebAssembly.Module> {
+    const response = await fetch("wasm/mentat-interop.wasm");
+    const buffer = await response.arrayBuffer();
+
+    return WebAssembly.compile(buffer);
+}
+
+async function compileMentat(mentatLines: [string]): Promise<string> {
+    const mentatWasm = await loadMentatInterface();
+
+    const instance = await WebAssembly.instantiate(mentatWasm);
+
+    const translateMentatProgram = instance.exports.c_trans_mentat_program as (mentatLines: [string]) => string;
+
+    return translateMentatProgram(mentatLines);
+}
 
