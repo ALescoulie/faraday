7bc167a8b98adc2dcf31879fa7e2578345615b7e
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-03-13 14:45:14 -0700

    start developing UI

--- src/app/faraday.tsx
+++ src/app/faraday.tsx
@@ -1,8 +1,274 @@
-import React from 'react';
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import CircleIcon from '@mui/icons-material/Circle';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import CircleOutlined from '@mui/icons-material/CircleOutlined';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MenuIcon from '@mui/icons-material/Menu';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import {styled, useTheme} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+
+
+interface InputLineProps {
+    index: number;
+    content: string;
+    graph: boolean;
+
+}
+
+
+// Toggels graphing a thing on and off
+function setChecked(event: React.ChangeEvent<HTMLInputElement>,
+                    index: number) {
+    if (event.target.checked) {
+        console.log("checked");
+    } else {
+        console.log("unchecked");
+    }
+};
+
+
+// Deletes line when delete button pressed
+function inputLineDelete (index: number) {
+    console.log(`hit delete button for index ${index}`);
+}
+
+function InputLineItem (props: InputLineProps) {
+    const {index, content, graph, ...other} = props;
+
+    return (
+        <ListItem
+            key={index}
+            secondaryAction={
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            }
+            disablePadding
+        >
+            <ListItemButton role={undefined} onClick={() => inputLineDelete(index)} dense>
+                <ListItemIcon>
+                    <Checkbox
+                        icon={<CircleIcon/>}
+                        checkedIcon={<CircleOutlined />}
+                        defaultChecked
+                        disabled={graph}
+                        onChange={(event) => setChecked(event, index)}
+                    />
+                </ListItemIcon>
+                <ListItemText
+                    id={`input${index}`}
+                    primary={<TextField id={`input ${index}`} multiline variant="filled" />}
+                >
+                </ListItemText>
+            </ListItemButton>
+        </ListItem>    
+    );
+}
+
+
+const drawerWidth = 20
+
+
+
+
+
+const DrawerHeader = styled('div')(({ alignment }) => ({
+    display: 'flex',
+    alignItems: alignment,
+    justifyContent: 'flex-end'
+}));
+
+function InputDrawer (open, drawerCloseHandler) {
+
+    return (
+        <Drawer
+            sx={{
+                width: `${drawerWidth}%`,
+                flexShrink: 0
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+        >
+            <DrawerHeader>
+                <IconButton onClick={drawerCloseHandler}>
+                    <ChevronLeftIcon />
+                </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <List>
+                <InputLineItem index={0} graph={false} content="" />
+            </List>
+        </Drawer>
+    )
+}
+
+
+
+
+function FaradayUI () {
+
+    const [open, setOpen] = React.useState(false); 
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+      open?: boolean;
+    }>(({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}%`,
+      ...(open && {
+        transition: theme.transitions.create('margin', {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    }));
+
+
+    const AppBar = styled(MuiAppBar, {
+      shouldForwardProp: (prop) => prop !== 'open',
+    })<AppBarProps>(({ theme, open }) => ({
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      ...(open && {
+        width: `calc(100% - ${drawerWidth}%)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      }),
+    }));
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap component="div">
+                    Faraday
+                </Typography>
+            </Toolbar>
+            </AppBar>
+            {InputDrawer(open, handleDrawerClose)}
+            <Main open={open}>
+                <DrawerHeader />
+                <Typography paragraph>
+                    insert canvas here
+                </Typography>
+            </Main>
+        </Box>
+    )
+}
+
+
+interface TabPannelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+
+function CustomTabPanel (props: TabPannelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{p: 3}}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 
 function Faraday() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
     return (
-        <h1>Faraday initial frontend build.</h1>
+        <Box sx={{ width: '100%' }}>
+            <Box xs={{ borderBottom: 1, borderColor: 'grey' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="basic tabs example"
+                    variant="scrollable"
+                >
+                  <Tab label="Item One" {...a11yProps(0)} />
+                  <Tab label="Item Two" {...a11yProps(1)} />
+                  <Tab label="Item Three" {...a11yProps(2)} />
+                  <Tab label="New Tab" {...a11yProps(3)} />
+                </Tabs>
+            </Box>
+          <CustomTabPanel value={value} index={0}>
+            {FaradayUI()}
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            {FaradayUI()}
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            {FaradayUI()}
+          </CustomTabPanel>
+        </Box>
     );
 }
 
