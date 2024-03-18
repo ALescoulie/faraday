87f23de161bc716e384350e0b6db1e4c5866b0c6
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-03-17 21:13:46 -0700

    test that all wasm functions are present

--- test/mentat-wasm/wasm_pkg.test.ts
+++ test/mentat-wasm/wasm_pkg.test.ts
@@ -24,6 +24,11 @@ describe("Testing that mentat-interop.wasm exports methods correctly", () => {
     test("Exports c_trans_mentat_program", () => {
         inspectExports().then(exportNames => {
             expect(exportNames.includes('c_trans_mentat_program'));
+            expect(exportNames.includes('malloc'));
+            expect(exportNames.includes('free'));
+            expect(exportNames.includes('getString'));
+            expect(exportNames.includes('getStringLen'));
+            expect(exportNames.includes('freeStableCStringLen'));
         }).catch(error => {
             throw error;
         });
